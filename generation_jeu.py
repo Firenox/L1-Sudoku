bad6c335933a1c8c9cd1d4e4177bfbdba12c5324
Looloo <looloo5876@gmail.com>
2026-02-21 13:43:18 +0100

    essai 8

--- generation_jeu.py
+++ generation_jeu.py
@@ -37,7 +37,7 @@ class Sudoku :
                     print(self.grille[i][j], end = "   ")
             print()    
     
-    def ajout_des_trois(self):
+    def ajout(self):
         chiffres = random.randint(1, 9)
         for i in range(3):
             for j in range(3):
