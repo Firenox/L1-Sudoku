02da770b598252cb027a069cb53a76de60c19523
Firenox <vahebaghda@gmail.com>
2026-02-23 15:35:39 +0100

    Création Fenêtre Tkinter - Après animation > Apparition texte

--- fenetre_tkinter.py
+++ fenetre_tkinter.py
@@ -13,15 +13,16 @@ racine.resizable(0, 0) # https://stackoverflow.com/questions/36575890/how-to-set
 '''
 Placer image avec PhotoImage pour permettre superposition
 https://openclassrooms.com/forum/sujet/creation-plusieurs-images-dans-tkinter-1
-
+ 
 Ajouter fond_id avant 'canvas.create_image(...)' pour pouvoir la supprimer
 https://stackoverflow.com/questions/68121138/python-tkinter-canvas-delete
 
 '''
 
 canvas = tk.Canvas(racine, width=1200, height=700, bg="black")
-fond = tk.PhotoImage(file='Medias/fond_1.png')
+fond = tk.PhotoImage(file="Medias/fond_1.png")
 fond_id = canvas.create_image(0, 0, image=fond, anchor='nw')
+
 
 def fond_supprimer():
     canvas.delete(fond_id)
@@ -44,10 +45,10 @@ def intencite():            # Source : Mon talent
     listes_id=[[None]*15 for i in range(4)]
 
     # Sorti de la boucle = infiniment plus rapide
-    logo_S = tk.PhotoImage(file='Medias\Logo par lettres\SVB Plays 10prct - S.png') # le S du logo avec 10% d'opacité
-    logo_V = tk.PhotoImage(file='Medias\Logo par lettres\SVB Plays 10prct - V.png') # le V du logo avec 10% d'opacité
-    logo_B = tk.PhotoImage(file='Medias\Logo par lettres\SVB Plays 10prct - B.png') # le B du logo avec 10% d'opacité
-    logo_Plays = tk.PhotoImage(file='Medias\Logo par lettres\SVB Plays 10prct - Plays.png') # le Plays du logo avec 10% d'opacité
+    logo_S = tk.PhotoImage(file='Medias/Logo par lettres/SVB Plays 10prct - S.png') # le S du logo avec 10% d'opacité
+    logo_V = tk.PhotoImage(file='Medias/Logo par lettres/SVB Plays 10prct - V.png') # le V du logo avec 10% d'opacité
+    logo_B = tk.PhotoImage(file='Medias/Logo par lettres/SVB Plays 10prct - B.png') # le B du logo avec 10% d'opacité
+    logo_Plays = tk.PhotoImage(file='Medias/Logo par lettres/SVB Plays 10prct - Plays.png') # le Plays du logo avec 10% d'opacité
 
     logo_S, logo_V, logo_B, logo_Plays = logo_S.subsample(2, 2), logo_V.subsample(2, 2), logo_B.subsample(2, 2), logo_Plays.subsample(2, 2)
 
@@ -104,6 +105,8 @@ def intencite():            # Source : Mon talent
     logo = logo.subsample(12, 12)
     canvas.create_image(1100, 0, anchor="nw", image=logo) # Anchor >> _tkinter.TclError: bad anchor position "tkinter.NW": must be n, ne, e, se, s, sw, w, nw, or center
     #appeler une fonction qui affiche les boutons
+    label = tk.Label(racine, bg=None, text = "Un magnifique texte",  font=("Helvetica", "28"))
+    label.place(x=500, y=350)
 
 
 canvas.pack()
