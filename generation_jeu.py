d0979123de14f5c6b6fb907940d7d40660169f87
Looloo <looloo5876@gmail.com>
2026-04-04 18:54:48 +0200

    finie la méthode ajouter

--- generation_jeu.py
+++ generation_jeu.py
@@ -37,7 +37,7 @@ class Sudoku :
                     self.grille[i][j] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
                 
                 
-                
+    #affiche
     def affichage(self):
         for i in range(len(self.grille)):
             for j in range(len(self.ligne1)):
@@ -47,19 +47,20 @@ class Sudoku :
                     print(self.grille[i][j], end = "   ")
             print()    
             
-            
-            
+    
+    
     def est_valide(self, chiffre, i, j, k):
         if chiffre in self.grille[i][j]:
             return False
         
-        # k//3 la position 
-        debut = (k // 3) 
+        # k//3 la position / vérife en ligne
+        debut = (k // 3) * 3
         fin = debut + 3
-        chiffres_ligne = [self.grille[i][0][debut:fin], self.grille[i][1][debut:fin], self.grille[i][2][debut:fin]]
+        chiffres_ligne = self.grille[i][0][debut:fin] + self.grille[i][1][debut:fin] + self.grille[i][2][debut:fin]
         if chiffre in chiffres_ligne:
             return False
 
+        # vérife en colonne
         col = k % 3
         chiffres_colonne = []
         for ligne_bloc in range(3):
@@ -74,39 +75,69 @@ class Sudoku :
 
 
     def ajouter(self, i, j, k):
-        chiffres = random.randint(1, 9)
-        if k in [1,2,3]:
-            chiffres = random.randint(1, 9)
-            if chiffres not in self.grille[i][j+1][:3] and chiffres not in self.grille[i][j+3][:3] and chiffres not in self.grille[i][j+6][:3]:
-                self.grille[i][j][k] = chiffres
-                self.ajouter(i, j, k+1)
-            else :
-                self.ajouter(i, j, k)
-        if k in [4,5,6]:
-            chiffres = random.randint(1, 9)
-            if chiffres not in self.grille[i][j+1][3:7] and chiffres not in self.grille[i][j+3][3:7] and chiffres not in self.grille[i][j+6][3:7]:
-                self.grille[i][j][k] = chiffres
-                self.ajouter(i, j, k+1)
-            else :
-                self.ajouter(i, j, k)
-        if k in [7,8,9] :
-            chiffres = random.randint(1, 9)
-            if chiffres not in self.grille[i][j+1][7:] and chiffres not in self.grille[i][j+3][7:] and chiffres not in self.grille[i][j+6][7:]:
-                self.grille[i][j][k] = chiffres
-                self.ajouter(i, j, k+1)
-            else :
-                self.ajouter(i, j, k)
         if k == 9:
             j += 1 
             self.ajouter(i, j, 0)
-        if j == 3 :
+        elif j == 3:
             i += 1
             self.ajouter(i, 0, 0)
             
+        if self.grille[i][j][k] != 0:
+            return self.ajouter(i, j, k)
+        
+        chiffres = random.randint(1, 10)
+        if k in [0,1,2]:
+            chiffres = random.randint(1, 10)
+            if self.est_valide(chiffres, i, j, k):
+                self.grille[i][j][k] = chiffres
+                self.ajouter(i, j, k+1)
+            else :
+                self.ajouter(i, j, k)
+        if k in [3,4,5]:
+            chiffres = random.randint(1, 10)
+            if self.est_valide(chiffres, i, j, k):  
+                self.grille[i][j][k] = chiffres
+                self.ajouter(i, j, k+1)
+            else :
+                self.ajouter(i, j, k)
+        if k in [6,7,8] :
+            chiffres = random.randint(1, 10)
+            if self.est_valide(chiffres, i, j, k):
+                self.grille[i][j][k] = chiffres
+                self.ajouter(i, j, k+1)
+            else :
+                self.ajouter(i, j, k)
+        
+    
+    def ajouter1(self, i, j, k):
+        if k == 9:
+            k = 0
+            j += 1
+        if j == 3:
+            j = 0
+            i += 1
+        #fin
+        if i == 3:
+            return True 
+    
+        # vérif si la case est déjà remplie (Diagonales)
+        if self.grille[i][j][k] != 0:
+            return self.ajouter1(i, j, k + 1)
+    
+        # remplie la case
+        for num in range(1, 10):
+            if self.est_valide(num, i, j, k):
+                self.grille[i][j][k] = num
+                if self.ajouter1(i, j, k + 1):
+                    return True
+                # zéro si ça  marche pas 
+                self.grille[i][j][k] = 0 
+                
+        return False
         
 sudo = Sudoku()
 sudo.trois_case_en_diagonal()
-sudo.ajouter(0,0,0)
+sudo.ajouter1(0,0,0)
 
 sudo.affichage()
 
