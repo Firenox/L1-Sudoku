437eaf7f9d2867445d95c454ea3b17f0457c1bd7
Firenox <vahebaghda@gmail.com>
2026-04-20 11:36:10 +0200

    Main | Avancement

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import fenetre_tkinter
 import sauvegardes
 import aide_au_jeu
 import generation_jeu_vide
-import Maquette_tkinter_SylvainM
 
 nombre_de_parties_jouees = 0
 meilleur_temps = 0.0
 
 matrice=(generation_jeu_vide.matrice())
-fenetre_tkinter.lancer_fenetre_1()
+
+def executer():
+    fenetre_tkinter.lancer_fenetre_1()
