c382579384b58e3712d15b5d2e307ca090239ebb
Looloo <looloo5876@gmail.com>
2026-04-20 11:27:07 +0200

    fini les 2 fonctions aides

--- aide_au_jeu.py
+++ aide_au_jeu.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+# HEAD
 #Ici, on créer une "IA" qui aide le joueur quand il en a besoin
 #Si un nombre est trouvable de manière logique : On essaye de l'expliquer
 
@@ -9,11 +9,29 @@
 - si l'utilisateur c'est trompée : on lui indique l'erreur ,on compte ces erreurs ,et on choisie 1 case 
 du bloc/colone/ligne de la case par lequelle on remplace par 1 des chiffres 
 
-
 """
-=======
+
 '''
 Ici, on créer une "IA" qui aide le joueur quand il en a besoin
 Si un nombre est trouvable de manière logique : On essaye de l'expliquer
 '''
->>>>>>> ec4ec2270f2e8178d5987fa28d6c1b6bb70b20c3
+
+import random
+import main
+main.matrice
+
+
+def aide(sudo):
+    case_vide = []
+    for i in range(9):
+        for j in range(9):
+            if sudo[1][i][j] == " ":
+                case_vide.append(i,j)
+    random.random(case_vide)
+    sudo[1][i][j] = sudo[0][i][j]
+                    
+def aide_fort(sudo, nombre):
+    for i in range(9):
+        for j in range(9):
+            if sudo[1][i][j] == nombre:
+                sudo[1][i][j] = sudo[0][i][j]
