85ebc0d07634af0911a68b93b39ad817f7250c80
Senzzin <hiochi3d@gmail.com>
2026-04-20 11:18:45 +0200

    ajouter difficulter

--- aide_au_jeu.py
+++ aide_au_jeu.py
@@ -1,2 +1,12 @@
 #Ici, on créer une "IA" qui aide le joueur quand il en a besoin
 #Si un nombre est trouvable de manière logique : On essaye de l'expliquer
+
+
+"""
+2 option : 
+- un bouton aide avec lequel l'utilisateur interagie , il doit choisire une case avec lequelle il veut l'aide.
+- si l'utilisateur c'est trompée : on lui indique l'erreur ,on compte ces erreurs ,et on choisie 1 case 
+du bloc/colone/ligne de la case par lequelle on remplace par 1 des chiffres 
+
+
+"""
