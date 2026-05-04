8f4e1aedb48d61b16dc14a0db7b5e30bbab5af8a
Firenox <vahebaghda@gmail.com>
2026-05-04 13:30:49 +0200

    Sauvegarde | Léger avancement

--- aide_au_jeu.py
+++ aide_au_jeu.py
@@ -10,9 +10,6 @@ Si un nombre est trouvable de manière logique : On essaye de l'expliquer.
 import random
 # on utilise etat_de_jeu.matrice au lieu de main.matrice :
 import etat_de_jeu 
-etat_de_jeu.creer_matrice(1)
-print(etat_de_jeu.matrice)
-
 def aide_normal(sudo):
     case_vide = []
     for i in range(9):
--- sauvegardes.py
+++ sauvegardes.py
@@ -2,12 +2,22 @@
 # Solution | Générer un fichier txt avec les données dedans.
 
 # Essai
-def sauvegarder(grille):
-    f_output = open("sauvegarde.txt", "w")
-    
-    for ligne in grille:
-        for valeur in ligne:
-            f_output.write(str(valeur) + " ")
-        f_output.write("\n")
-    
-    f_output.close()
+import time
+def sauvegarder(matrice, donnes_jeu):
+    sauvegarde = open("sauvegarde.txt", "w")
+
+    # time.time() = Heure en bits toujours unique. Int pour garder jusqu'aux secondes
+    sauvegarde.write("\n" + [matrice[0], matrice[1], donnes_jeu[0], donnes_jeu[1], donnes_jeu[2]])
+    sauvegarde.close()
+
+def ouvrir():
+    sauvegarde = open("sauvegarde.txt", "w")
+    liste = []
+
+    line = sauvegarde.readline()
+
+    while line != "":
+        liste += line
+        line = sauvegarde.readline()
+
+    return liste
