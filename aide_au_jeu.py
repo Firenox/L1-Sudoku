9c0e5779063f385decd0d3ade81c4c6753e879c0
Looloo <looloo5876@gmail.com>
2026-05-07 15:39:06 +0200

    Correction aide au jeu

--- aide_au_jeu.py
+++ aide_au_jeu.py
@@ -10,18 +10,27 @@ Si un nombre est trouvable de manière logique : On essaye de l'expliquer.
 import random
 # on utilise etat_de_jeu.matrice au lieu de main.matrice :
 import etat_de_jeu 
-def aide_normal(sudo):
+
+#Aide normal: qui donne au hasard un nombre d'une case vide dans le sudoku 
+def aide_normal():
     case_vide = []
     for i in range(9):
         for j in range(9):
-            if sudo[1][i][j] == " ":
-                case_vide.append(i,j)
-    random.random(case_vide)
-    sudo[1][i][j] = sudo[0][i][j]
-                    
-def aide_fort(sudo, nombre):
+            if etat_de_jeu.matrice[1][i][j] == "":
+                case_vide.append((i,j))
+    
+    i = random.randint(0, len(case_vide) -1)
+    print(case_vide)
+    etat_de_jeu.matrice[1][case_vide[i][0]][case_vide[i][1]] == etat_de_jeu.matrice[0][case_vide[i][0]][case_vide[i][1]]
+    
+    etat_de_jeu.matrice[1][i][j] = etat_de_jeu.matrice[0][i][j]
+
+
+#Aide fort: qui donne tout les emplacements d'un chiffre demander dans la matrice
+def aide_fort():
+    nombre = random.randint(1, 9)
+    print(nombre)
     for i in range(9):
         for j in range(9):
-            if sudo[1][i][j] == nombre:
-                sudo[1][i][j] = sudo[0][i][j]
-
+            if etat_de_jeu.matrice[0][i][j] == nombre:
+                etat_de_jeu.matrice[1][i][j] = etat_de_jeu.matrice[0][i][j]
